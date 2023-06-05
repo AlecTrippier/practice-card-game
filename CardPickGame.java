f522da1437072ba18ae35a9a25caab71ad9b6e74
Laravel <webiten3@gmail.com>
2023-06-05 17:59:48 +0900

    CardPickGameクラスの作成

---
+++ CardPickGame.java
@@ -0,0 +1,15 @@
+public class CardPickGame{
+
+    private int maxBetCoin = 100; //最大ベット枚数
+    private int deckSetCount = 2; //カードセット数
+    private int possessionCoin ; //所持コイン数
+
+    public CardPickGame(int possessionCoin) {
+        this.possessionCoin = possessionCoin;
+    }
+
+    public void execute() {
+
+    }
+
+}
