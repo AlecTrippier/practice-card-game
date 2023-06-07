c6d652bcdb1626810ebd699f83aea436db0d36d2
Laravel <webiten3@gmail.com>
2023-06-07 10:27:42 +0900

    カード数の大小比較判定機能の実装

--- ExeTest.java
+++ ExeTest.java
@@ -1,16 +1,16 @@
 public class ExeTest {
 
     public static void main(String[] args) {
-        CardPickGame game = new CardPickGame(400); //インスタンス生成時に所持コイン枚数を設定する
+       // CardPickGame game = new CardPickGame(400); //インスタンス生成時に所持コイン枚数を設定する
         HighAndLowGame game2 = new HighAndLowGame(100,200);
-        String inputString = GameUtils.getInputString();
-        int inputInt = GameUtils.getInputInt();
-        int randomInt = GameUtils.getRandomInt(5);
-        boolean result = GameUtils.checkPattern("test");
-        System.out.println(inputString);
-        System.out.println(inputInt);
-        System.out.println(randomInt);
-        System.out.println(result);
+       // String inputString = GameUtils.getInputString();
+       // int inputInt = GameUtils.getInputInt();
+        //int randomInt = GameUtils.getRandomInt(5);
+        //boolean result = GameUtils.checkPattern("test");
+        // System.out.println(inputString);
+        // System.out.println(inputInt);
+        // System.out.println(randomInt);
+        // System.out.println(result);
     }
 
 }
--- HighAndLowGame.java
+++ HighAndLowGame.java
@@ -19,12 +19,17 @@ public class HighAndLowGame {
         cardList = getCard(cardList);
         System.out.println(cardList);
 
+        cardList = getCard(cardList);
+        System.out.println(cardList);
+        boolean result = judgeCard(cardList, true); // 第二引数は「High」を選択した状態
+        System.out.println(result);
+
     }
 
     private List<Integer> getCard(List<Integer> cardList) {
 
         List<List<Integer>> setDeck = new ArrayList<List<Integer>>();
-        List<Integer> onePair = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9 ,10));
+        List<Integer> onePair = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
         for (int i = 0; i < this.deckSetCount; i++) {
             setDeck.add(i, onePair);
@@ -57,5 +62,33 @@ public class HighAndLowGame {
 
         return cardList;
     }
+
+    private boolean judgeCard(List<Integer> cardList, boolean pickChoice) { // pickChoice → High:true , Low:false
+
+    
+            /**
+                    * 1. cardListの最後2つの数字を取り出す。
+                    * 最後の数字を[A], 1つ前を[B]とする
+                    */
+            int num = cardList.size();
+            int lastCard = cardList.get(num - 1); //[A]
+            int penultimateCard = cardList.get(num - 2); //[B]
+           // 2. 結果を判定する
+            if (lastCard == penultimateCard) { // [A] = [B]の場合
+            // 負けとなり、falseを返却する
+            return false;
+           }
+           /**
+                    * [A] > [B]の場合
+                    *   勝ちとなり、trueを返却する
+                    * [A] < [B]の場合
+                    *   負けとなり、falseを返却する
+                    */
+           boolean flg = (lastCard > penultimateCard) ? true : false; // ここが基準
+           if (pickChoice == flg) {
+           return true;
+           }
+           return false;
+           }
 
 }
