7f6c5990322dd8e925628deac1bf1745af16d8dd
Laravel <webiten3@gmail.com>
2023-06-07 13:34:07 +0900

    CardPickGameクラスに処理を追加する

--- Playing.java
+++ Playing.java
@@ -1,28 +1,46 @@
-public class Playing {
+import java.util.Scanner;
 
-    private static String username = ""; // ユーザー名
-    private static int possessionCoin = 1000; // 所持コイン（初期値）
+public class Playing {
 
     public static void main(String[] args) {
         System.out.println("Welcome !");
         System.out.println("Enter your username");
+        Scanner scanner = new Scanner(System.in);
+        String username = scanner.nextLine();
+        System.out.println("Hello " + username);
 
-        username = GameUtils.getInputString();
+        int earnedCoinCount = 1000;
+        HighAndLowGame game = new HighAndLowGame(earnedCoinCount, 2);
 
-        if (!GameUtils.checkPattern(username)) {
-            System.out.println("Does not match condition of the username");
-            main(args); // ユーザー名入力の処理に戻る
-        } else {
-            System.out.println("Hello " + username);
+        while (true) {
+            System.out.println("You have " + earnedCoinCount + " Coin, Start the game? y / n");
+            String startChoice = scanner.nextLine();
 
-            CardPickGame game = new CardPickGame(possessionCoin);
-            possessionCoin = execute(game);
+            if (startChoice.equalsIgnoreCase("y")) {
+                System.out.println("Please bet Coin 1 ~ 100");
+                int betCoin = Integer.parseInt(scanner.nextLine());
 
-            System.out.println(username + " Possession : " + possessionCoin + "Coin");
+                if (betCoin < 1 || betCoin > 100) {
+                    System.out.println("Invalid input. Please enter a valid bet Coin between 1 and 100.");
+                    continue;
+                }
+
+                int resultCoin = game.execute();
+                earnedCoinCount += resultCoin;
+                System.out.println("Your winCoin is " + resultCoin);
+
+                if (resultCoin == 0) {
+                    System.out.println("You lose");
+                } else {
+                    System.out.println("You got " + resultCoin + " Coin !!");
+                }
+            } else if (startChoice.equalsIgnoreCase("n")) {
+                break;
+            } else {
+                System.out.println("Invalid input. Please enter 'y' or 'n'.");
+            }
         }
-    }
 
-    private static int execute(CardPickGame game) {
-        return game.execute();
+        System.out.println(username + " Possession : " + earnedCoinCount + " Coin");
     }
 }
